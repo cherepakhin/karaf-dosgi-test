255e28065bed38ad8cd00914bc845f90ce1bbe0c
Черепахин Василий <vasi.che@gmail.com>
2021-04-06 18:13:30 +0500

    Версия 1.0.2. Убраны аннотации. Подвязан blueprint.

--- service-simple/src/main/java/ru/perm/v/dosgi/simple/service/JacksonIntent.java
+++ service-simple/src/main/java/ru/perm/v/dosgi/simple/service/JacksonIntent.java
@@ -3,7 +3,6 @@ package ru.perm.v.dosgi.simple.service;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.cxf.dosgi.common.api.IntentsProvider;
-import org.osgi.service.component.annotations.Component;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,11 +21,6 @@ import java.util.List;
  * )
  * </pre>
  */
-@Component(
-        property = {
-                "org.apache.cxf.dosgi.IntentName=jackson"
-        }
-)
 @Slf4j
 public class JacksonIntent implements IntentsProvider {
 
--- service-simple/src/main/java/ru/perm/v/dosgi/simple/service/PeopleServiceImpl.java
+++ service-simple/src/main/java/ru/perm/v/dosgi/simple/service/PeopleServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.perm.v.dosgi.simple.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.osgi.service.component.annotations.Component;
 import ru.perm.v.dosgi.simple.api.People;
 import ru.perm.v.dosgi.simple.api.PeopleService;
 
@@ -9,20 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@Component(
-        immediate = true,
-        name = "PeopleService",
-        property = {
-                "service.exported.interfaces=ru.perm.v.dosgi.simple.api.PeopleService",
-                "service.exported.configs=org.apache.cxf.rs",
-                "org.apache.cxf.rs.address=/peoples",
-                // выбор преобразователя в json.
-                // Имя jackson определено в ru.perm.v.dosgi.simple.service.JacksonIntent
-                "service.exported.intents=jackson",
-                // By default CXF will favor the default json provider
-                "cxf.bus.prop.skip.default.json.provider.registration=true"
-        }
-)
 public class PeopleServiceImpl implements PeopleService {
     Map<Integer, People> peoples = new HashMap<>();
 
